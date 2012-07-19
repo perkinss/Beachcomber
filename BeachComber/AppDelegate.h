c6b450c3175a2227ce103a0187fa32ecf2501395
Jeff Proctor <jrproctor@gmail.com>
2012-07-18 22:57:26 -0700

    Initial Commit

---
+++ BeachComber/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BeachComber
+//
+//  Created by Jeff Proctor on 12-07-18.
+//  Copyright (c) 2012 University of British Columbia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
